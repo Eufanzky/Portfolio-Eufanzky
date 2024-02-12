c5caabe0926f4e91535893a91fb2180259834f5a
eufanzky <eufanzky@gmail.com>
2024-02-12 14:26:14 -0400

    added new hero background and new styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <span className="text-[#4361EE] font-medium mb-4">Your Name</span>
             <input
               type="text"
               name="name"
@@ -48,7 +48,7 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
+            <span className="text-[#4361EE] font-medium mb-4">Your email</span>
             <input
               type="email"
               name="email"
@@ -59,7 +59,7 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
+            <span className="text-[#4361EE] font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
               name="message"
@@ -72,7 +72,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-[#4361EE] font-bold shadow-md shadow-primary"
           >
             {loading ? "Sending..." : "Send"}
           </button>
